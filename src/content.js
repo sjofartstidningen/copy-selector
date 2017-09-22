41a9e0a0a283615766ac83e119c667ee4cbdf538
Adam Bergman <adam@fransvilhelm.com>
2017-09-22 12:08:17 +0200

    :zap: Improving code

--- src/content.js
+++ src/content.js
@@ -1,23 +1,25 @@
 import createElement from './lib/createElement';
 import createIcon from './lib/createIcon';
 import Messanger from './lib/Messanger';
+import Selection from './lib/Selection';
 
 const selectorToCopy = process.env.SELECTOR;
+const elToCopy = document.querySelector(selectorToCopy);
 
 const createContainer = () => createElement('div', 'container');
 const createButton = () => createElement('button', 'button');
 const createMessage = () => createElement('span', 'message');
+const createSelection = () => createElement('div', 'selection');
 const createTextarea = () => createElement('textarea', 'textarea-hidden');
 
 const messenger = new Messanger();
+const selectionOverlay = new Selection();
 
 const onClick = () => {
   try {
-    const textarea = createTextarea();
-    const elToCopy = document.querySelector(selectorToCopy);
-
     if (!elToCopy) throw new Error('Could not find element to copy');
 
+    const textarea = createTextarea();
     const html = elToCopy.outerHTML;
 
     textarea.value = html.trim();
@@ -25,30 +27,55 @@ const onClick = () => {
     textarea.select();
 
     const success = document.execCommand('Copy');
-    messenger.show(
-      success ? 'Copied' : 'Could not copy',
-      success ? 'success' : 'warning',
-    );
+
+    window.requestAnimationFrame(() => {
+      messenger.show(
+        success ? 'Copied' : 'Could not copy',
+        success ? 'success' : 'warning',
+      );
+    });
+
     document.body.removeChild(textarea);
   } catch (e) {
-    messenger.show('An error occured, see log', 'warning');
     console.error(e);
+    window.requestAnimationFrame(() => {
+      messenger.show('An error occured, see log', 'warning');
+    });
   }
 };
+
+const onEnter = () => {
+  window.requestAnimationFrame(() => {
+    const box = elToCopy.getBoundingClientRect();
+    selectionOverlay.show(box);
+  });
+};
+
+const onLeave = () => {
+  window.requestAnimationFrame(() => {
+    selectionOverlay.hide();
+  });
+}
 
 function init() {
   const container = createContainer();
   const button = createButton();
   const icon = createIcon();
   const message = createMessage();
+  const selection = createSelection();
+
   messenger.addEl(message);
+  selectionOverlay.addEl(selection)
 
   button.addEventListener('click', onClick);
+  button.addEventListener('mouseenter', onEnter);
+  button.addEventListener('mouseleave', onLeave);
 
   button.appendChild(icon);
   container.appendChild(button);
   container.appendChild(message);
   document.body.appendChild(container);
+  document.body.appendChild(selection);
 }
 
 init();
---
+++ src/lib/Selection.js
@@ -0,0 +1,31 @@
+import { addClass, removeClass } from './createElement';
+
+export default class Selection {
+  constructor() {
+    this.el = null;
+  }
+
+  addEl = el => {
+    this.el = el;
+    addClass(this.el, 'hidden');
+  };
+
+  show = box => {
+    if (this.el != null) {
+      addClass(this.el, 'show');
+      removeClass(this.el, 'hidden');
+
+      this.el.style.top = `${box.top}px`;
+      this.el.style.left = `${box.left}px`;
+      this.el.style.width = `${box.width}px`;
+      this.el.style.height = `${box.height}px`;
+    }
+  }
+
+  hide = () => {
+    if (this.el != null) {
+      removeClass(this.el, 'show');
+      addClass(this.el, 'hidden');
+    }
+  }
+}
