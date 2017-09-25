1b993dbf2826ce49c35461e8df467139e939189c
Adam Bergman <adam@fransvilhelm.com>
2017-09-25 14:23:04 +0200

    :wrench: Update sass/css build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@
 set -e
 
 ./node_modules/.bin/rollup -c
-./node_modules/.bin/node-sass src/content.scss copy-selector/content.css
+node sass.config.js
 ./scripts/build-html.js
 ./scripts/build-icons.js
 ./scripts/build-manifest.js
